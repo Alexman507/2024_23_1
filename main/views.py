ce5e9d0f88984409a1e991ae797c570789ec07a2
SRE <Nerocross507@gmail.com>
2024-06-26 12:30:40 +0300

    copy from previous project

---
+++ main/views.py
@@ -0,0 +1,109 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms import inlineformset_factory
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from main.forms import ProductForm, VersionForm, VersionFormSet
+from main.models import Product, Contact, Order, Version
+
+
+class GetContextMixin:
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['version'] = Version.objects.all()
+        return context_data
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy("main:product_list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        response = super().form_valid(form)
+        return response
+
+
+class ProductListView(GetContextMixin, ListView):
+    model = Product
+
+
+class ProductDetailView(GetContextMixin, DetailView):
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        context['title'] = self.object.name
+        return context
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('main:product_list')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        product_formset = inlineformset_factory(Product, Version, form=VersionForm, formset=VersionFormSet, extra=1)
+        if self.request.method == "POST":
+            context_data["formset"] = product_formset(self.request.POST, instance=self.object)
+        else:
+            context_data["formset"] = product_formset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data["formset"]
+        if form.is_valid and formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form, formset=formset))
+
+
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    model = Product
+    success_url = reverse_lazy('main:product_list')
+
+
+class ContactListView(ListView):
+    model = Contact
+
+
+class OrderCreateView(CreateView):
+    model = Order  # Модель
+    fields = ('name', 'phone', 'message', 'created_at')  # Поля для заполнения при создании
+    success_url = reverse_lazy('main:product_list')  # Адрес для перенаправления после успешного создания
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        response = super().form_valid(form)
+        return response
+
+
+class VersionListView(ListView):
+    model = Version
+
+
+class VersionDetailView(DetailView):
+    model = Version
+
+
+class VersionCreateView(CreateView):
+    model = Version
+    form_class = VersionForm
+    success_url = reverse_lazy("main:versions")
+
+
+class VersionUpdateView(UpdateView):
+    model = Version
+    form_class = VersionForm
+    success_url = reverse_lazy("main:versions")
+
+
+class VersionDeleteView(DeleteView):
+    model = Version
+    success_url = reverse_lazy("main:versions")
---
+++ users/management/commands/csu.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        user = User.objects.create(
+            email='nerocross@gmail.com',
+            first_name='Admin',
+            last_name='Admin',
+            is_superuser=True,
+            is_staff=True,
+            is_active=True,
+        )
+
+        user.set_password('123')
+        user.save()
