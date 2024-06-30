9a0d29c30a3d8cb92502e64babf57af47f4bfb6f
NeroPC <Nerocross507@gmail.com>
2024-06-30 14:13:15 +0300

    create content-manager

--- users/management/commands/ccm.py
+++ users/management/commands/ccm.py
@@ -4,15 +4,17 @@ from users.models import User
 
 
 class Command(BaseCommand):
-    """Команда для создания модератора"""
+    """Контент-манагер ннада?"""
 
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='moderator@sky.pro',
+            email="content-manager@sky.pro",
+            first_name="Content-manager",
+            last_name="SkyPro",
             is_active=True,
             is_staff=False,
             is_superuser=False,
         )
 
-        user.set_password('123')
+        user.set_password("123")
         user.save()
